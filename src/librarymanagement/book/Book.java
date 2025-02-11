2d87077250b48a31a7a286ac028b39f6b8c6a1c1
AndrewLHellman <142917243+AndrewLHellman@users.noreply.github.com>
2025-02-11 17:20:00 -0600

    Add final copy constructor to Book class

--- src/librarymanagement/book/Book.java
+++ src/librarymanagement/book/Book.java
@@ -12,12 +12,28 @@ public class Book {
 		this.author = "Unknown";
 		this.ISBN = "Unknown";
 		this.price = 0.0;
-	};
+	}
 	
 	public Book (String title, String author, String ISBN, double price) {
 		this.title = title;
 		this.author = author;
 		this.ISBN = ISBN;
 		this.price = price;
-	};
+	}
+	
+	public Book(Book aBook) {
+		if (aBook == null) {
+			System.err.println("Cannot copy a 'null' object, creating a default book instead.");
+			this.title = "Unknown";
+			this.author = "Unknown";
+			this.ISBN = "Unknown";
+			this.price = 0.0;
+		}
+		else {
+			this.title = aBook.title;
+			this.author = aBook.author;
+			this.ISBN = aBook.ISBN;
+			this.price = aBook.price;
+		}
+	}
 }
