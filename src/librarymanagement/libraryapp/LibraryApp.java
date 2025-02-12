1de1370ebb1fbc8563ce9ff799412a34d8df6cee
SHenders2424 <s.henderson2424@gmail.com>
2025-02-12 13:42:27 -0600

    Setup imports and added the main function
    Imported the other classes into the LibraryApp class and added a main function with a created library.

--- src/librarymanagement/libraryapp/LibraryApp.java
+++ src/librarymanagement/libraryapp/LibraryApp.java
@@ -1,5 +1,11 @@
 package librarymanagement.libraryapp;
+import librarymanagement.book.Book;
+import librarymanagement.library.Library;
 
 public class LibraryApp {
 
+	public static void main(String[] args) {
+		Library library = new Library();
+		
+	}
 }
