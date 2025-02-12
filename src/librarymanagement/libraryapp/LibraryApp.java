f98ab3a34e5c737e40541a4cce1c42109a37f6fa
SHenders2424 <s.henderson2424@gmail.com>
2025-02-12 14:22:57 -0600

    Update LibraryApp.java
    Created the 3 books and used the setter functions to give them values

--- src/librarymanagement/libraryapp/LibraryApp.java
+++ src/librarymanagement/libraryapp/LibraryApp.java
@@ -7,5 +7,29 @@ public class LibraryApp {
 	public static void main(String[] args) {
 		Library library = new Library();
 		
+		Book book1 = new Book();
+		book1.setTitle("Clean Code");
+		book1.setAuthor("Robert C. Martin");
+		book1.setISBN("978-0135166307");
+		book1.setPrice(40.99);
+		
+		Book book2 = new Book();
+		book2.setTitle("Design Patterns");
+		book2.setAuthor("Erich Gamma");
+		book2.setISBN("978-0201633610");
+		book2.setPrice(50.99);
+		
+		Book book3 = new Book();
+		book3.setTitle("The Pragmattic Programmer");
+		book3.setAuthor("Andrew Hunt");
+		book3.setISBN("989-0135957059");
+		book3.setPrice(45.99);
+		
+		System.out.println("Adding Books...");
+		
+		library.addBook(book1);
+		library.addBook(book2);
+		library.addBook(book3);
+		
 	}
 }
