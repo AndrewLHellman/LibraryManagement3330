85c209fe8c3d569f860d2cde3fe6a94eb2e189e4
tyler-mitts <tmmwgb@umsystem.edu>
2025-02-12 12:20:52 -0600

    Create Library class and methods needed
    Library method:
    Sets max amount of books to 5 and initializes count to 0
    
    addBook method:
    Adds book from parameter into library if there is enough space
    
    removeBook method:
    Removes the book from library if a match from the parameter is found by setting to null
    
    searchByISBN method:
    Returns book if any of the books in library have matching ISBN values as the parameter
    
    displayBooks method:
    Prints the details of all the books

--- src/librarymanagement/library/Library.java
+++ src/librarymanagement/library/Library.java
@@ -1,1 +1,49 @@
+package librarymanagement.library;
+import librarymanagement.book.Book;
+
+public class Library {
+	
+	private Book[] books;
+	private int count;
+	
+	
+	public Library () {
+		this.books = new Book[5];
+		this.count = 0;
+	}
+	
+	public boolean addBook(Book book) {
+		if(this.count<5) {
+			this.books[++this.count] = book;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean removeBook(Book book) {
+		for(int i=0;i<this.count;i++) {
+			if(this.books[i] == book) {
+				this.books[i] = null;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Book searchByISBN(String ISBN) {
+		for(int i=0;i<this.count;i++) {
+			if(this.books[i].getISBN() == ISBN) {
+				return this.books[i];
+			}
+		}
+		return null;
+	}
+	
+	public void displayBooks() {
+		for(int i=0;i<this.count;i++) {
+			System.out.println(this.books[i].toString());
+		}
+	}
+	
+}
 
